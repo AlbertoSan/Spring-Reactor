e540eb1493abb34534664085bad4031e5f04ebf5
AlbertoSan <albertoensan@gmail.com>
2022-02-20 23:30:19 +0100

    Spring-Reactor: Creating Flux from List

--- src/main/java/com/starscodes/SpringReactor/SpringReactorApplication.java
+++ src/main/java/com/starscodes/SpringReactor/SpringReactorApplication.java
@@ -8,48 +8,58 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
 
-import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringReactorApplication implements CommandLineRunner {
 
-	private static final Logger log = LoggerFactory.getLogger(SpringReactorApplication.class);
-	public static void main(String[] args) {
-		SpringApplication.run(SpringReactorApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(SpringReactorApplication.class);
 
-	@Override
-	public void run(String... args) throws Exception {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactorApplication.class, args);
+    }
 
-		Flux<UsuarioDTO> nombres = Flux.just("Alberto Sánchez", "Pedro Guzman", "Andres Sierra", "Will Smith", "James Sidis")
-				.map(nombre -> {
-					String usuario[] = nombre.toUpperCase().split(" ");
-					return new UsuarioDTO(usuario[0], usuario[1]);
-				})
-				.filter(usuarioDTO -> usuarioDTO.getNombre().equalsIgnoreCase("will"))
-				.doOnNext(usuario -> {
-					if(usuario == null){
-						throw new RuntimeException("Nombres no pueden ser vacíos");
-					}
-					//Cada vez que se reciba un elemento lo mostraremos por consola
-					System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
-				})
-				.map(usuario -> {
-					String nombre = usuario.getNombre().toLowerCase();
-					String apellido = usuario.getApellido().toLowerCase();
-					usuario.setNombre(nombre);
-					usuario.setApellido(apellido);
-					return usuario;
-				});
+    @Override
+    public void run(String... args) throws Exception {
 
-		nombres.subscribe(e -> log.info(e.toString()),
-				error -> log.error(error.getMessage()),
-				new Runnable() {
-					@Override
-					public void run() {
-						log.info("Ha finalizado la ejecución del observable con éxito");
-					}
-				}
-		);
-	}
+        List<String> usuariosList = new ArrayList<>();
+        usuariosList.add("Alberto Sánchez");
+        usuariosList.add("Pedro Guzman");
+        usuariosList.add("Andres Sierra");
+        usuariosList.add("Will Smith");
+        usuariosList.add("James Sidis");
+
+        Flux<String> nombres = Flux.fromIterable(usuariosList);
+
+        Flux<UsuarioDTO> usuarios = nombres.map(nombre -> {
+                    String usuario[] = nombre.toUpperCase().split(" ");
+                    return new UsuarioDTO(usuario[0], usuario[1]);
+                })
+                .filter(usuarioDTO -> usuarioDTO.getNombre().equalsIgnoreCase("will"))
+                .doOnNext(usuario -> {
+                    if (usuario == null) {
+                        throw new RuntimeException("Nombres no pueden ser vacíos");
+                    }
+                    //Cada vez que se reciba un elemento lo mostraremos por consola
+                    System.out.println(usuario.getNombre().concat(" ").concat(usuario.getApellido()));
+                })
+                .map(usuario -> {
+                    String nombre = usuario.getNombre().toLowerCase();
+                    String apellido = usuario.getApellido().toLowerCase();
+                    usuario.setNombre(nombre);
+                    usuario.setApellido(apellido);
+                    return usuario;
+                });
+
+        usuarios.subscribe(e -> log.info(e.toString()),
+                error -> log.error(error.getMessage()),
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        log.info("Ha finalizado la ejecución del observable con éxito");
+                    }
+                }
+        );
+    }
 }
